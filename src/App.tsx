ea14036fb9d974aaddb0f747f1132e687ed675cb
Vika <vikapetrova8@gmail.com>
2025-05-30 23:55:44 +0300

    feat: add simple word card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type WordType = {
+    es: string
+    ru: string
 }
 
-export default App
+export function App() {
+    const [words] = useState<Array<WordType>>([
+        {es: 'Buenos dias!', ru: 'Доброе утро!'}
+    ])
+
+    const [showValue, setShowValue] = useState(false)
+
+    return (
+      <div onClick={() => setShowValue(!showValue)}>
+          {words.map((el, index) => <div key={index}>{showValue ? el.ru : el.es}</div>)}
+      </div>
+  )
+}
