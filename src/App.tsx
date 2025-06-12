b17a34762a79e41835e26cc1517a91e6f066403b
Vika <vikapetrova8@gmail.com>
2025-06-12 23:54:40 +0300

    добавление слов с поочерёдным вводом ru и es

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState} from "react";
+import {type ChangeEvent, useState} from "react";
 
 type WordType = {
     es: string
@@ -7,15 +7,45 @@ type WordType = {
 }
 
 export function App() {
-    const [words] = useState<Array<WordType>>([
+    const [words, setWords] = useState<Array<WordType>>([
         {es: 'Buenos dias!', ru: 'Доброе утро!'}
     ])
 
+    //если true - показать перевод на русский
     const [showValue, setShowValue] = useState(false)
 
+    const [rusValue, setRusValue] = useState('')
+    const [esValue, setEsValue] = useState('')
+    const [step, setStep] = useState<'ru' | 'es'>('ru')
+
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (step === 'ru') {
+            setRusValue(e.currentTarget.value)
+        } else {
+            setEsValue(e.currentTarget.value)
+        }
+    }
+
+    const addWord = () => {
+        if (step === 'ru') {
+            setStep('es')
+        } else {
+            setWords(prevState => ([...prevState, {es: esValue, ru: rusValue}]))
+            setStep('ru')
+            setRusValue('')
+            setEsValue('')
+        }
+    }
+    console.log(words)
+
     return (
-      <div onClick={() => setShowValue(!showValue)}>
-          {words.map((el, index) => <div key={index}>{showValue ? el.ru : el.es}</div>)}
-      </div>
-  )
+        <>
+            <div onClick={() => setShowValue(!showValue)}>
+                {words.map((el, index) => <div key={index}>{showValue ? el.ru : el.es}</div>)}
+            </div>
+            <input onChange={onChangeHandler} value={step === 'ru' ? rusValue : esValue}/>
+            <button onClick={addWord}>Сохранить</button>
+        </>
+    )
 }
